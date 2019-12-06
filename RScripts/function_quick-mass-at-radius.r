feb741604ef92c77f4bafdf76837d19ee38f043b
Gwendolyn Eadie <gwen.eadie@gmail.com>
2019-12-06 10:50:40 -0500

    wrote a quick function to calculate the mass at a given radius and the credible intervals. Requires calc.CredInt function

---
+++ RScripts/function_quick-mass-at-radius.r
@@ -0,0 +1,9 @@
+massfunc = function(chain, r, ...){
+  
+  # get the mass at that radius
+  Ms = M.r(r, pars = chain)
+  
+  # calculate the credible region for th emass at that radius, return in 1e12 solar mass unite
+  calc.CredInt(chain = as.matrix(Ms*2.325e9), colnum  = 1, ...)/1e12
+
+}
